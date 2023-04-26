8d8f01f9863619efce39bb4846913db207cf223c
arielabade <arielabadebandeira@gmail.com>
2023-04-25 22:52:17 -0300

    a

---
+++ urna-eletronica/src/main/java/com/mycompany/urna/eletronica/UrnaEletronica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.urna.eletronica;
+
+/**
+ *
+ * @author ARIEL
+ */
+public class UrnaEletronica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
