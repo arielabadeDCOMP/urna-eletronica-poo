dd50eeed3f7fc35a476cf3ff0423491218fe5a70
fernandamotad <131628531+fernandamotaDCOMP@users.noreply.github.com>
2023-04-29 16:57:17 -0300

    Update UrnaEletronica.java

--- urna-eletronica/src/main/java/com/mycompany/urna/eletronica/UrnaEletronica.java
+++ urna-eletronica/src/main/java/com/mycompany/urna/eletronica/UrnaEletronica.java
@@ -11,6 +11,7 @@ package com.mycompany.urna.eletronica;
 public class UrnaEletronica {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Tchau");
+        
     }
 }
