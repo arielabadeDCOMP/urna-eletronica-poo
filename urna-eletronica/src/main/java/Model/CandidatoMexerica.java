4eccfb5c2d5e382442b79d1bee40a8ea93380499
arielabade <arielabadebandeira@gmail.com>
2023-04-29 21:04:22 -0300

    oi

---
+++ urna-eletronica/src/main/java/Model/CandidatoMexerica.java
@@ -0,0 +1,18 @@
+
+package Model;
+
+
+/**
+ *
+ * @author ARIEL
+ */
+ class CandidatoMexerica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("OI");
+    }
+}
